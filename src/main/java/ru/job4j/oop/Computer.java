4269b59889dae2f429a5b300bbf9955e8ebc7bfb
Nurlan <001nurlan@gmail.com>
2024-01-23 21:06:01 +0600

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -9,9 +9,20 @@ public class Computer {
     public Computer() {
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
         this.cpu = cpu;
     }
 
@@ -26,7 +37,11 @@ public class Computer {
         computer.printInfo();
         Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
         comp.printInfo();
-        Computer therd = new Computer();
-        therd.printInfo();
+        Computer third = new Computer();
+        third.printInfo();
+        Computer fourth = new Computer(256, "AMD Ryzen 5 3600");
+        fourth.printInfo();
+        Computer fifth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        fifth.printInfo();
     }
 }
