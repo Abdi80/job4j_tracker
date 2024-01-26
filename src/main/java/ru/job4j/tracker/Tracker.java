6426c765187393d55ba847e97ba080b417492002
Nurlan <001nurlan@gmail.com>
2024-01-27 00:08:51 +0600

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,10 +47,11 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean result = index != -1;
+        if (result) {
             items[index].setName(item.getName());
         }
-        return index != -1;
+        return result;
     }
 
     public void delete(int id) {
