0a468b4b168fe2e1540126c0229d18c0c34a5451
Nurlan <001nurlan@gmail.com>
2024-01-27 13:44:30 +0600

    3. Массивы и модели. [#174892 #476680]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -23,7 +23,7 @@ public class Library {
         }
         System.out.println("Search Clean code");
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " quantity pages - " + book.getPages());
             }
         }
