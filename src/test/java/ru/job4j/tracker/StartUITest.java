73649b2ab7e4e0979ccf941062a9f5c0043123b6
Nurlan <001nurlan@gmail.com>
2024-02-09 21:26:15 +0600

    Именование классов реализующих интерфейс [#505124]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,7 +12,7 @@ class StartUITest {
         Input input = new MockInput(new String[] {"0", "Item name", "1"});
         Tracker tracker = new Tracker();
         Output output = new ConsoleOutput();
-        UserAction[] actions = {new CreateAction(output), new ExitAction(output)};
+        UserAction[] actions = {new Create(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
@@ -23,7 +24,7 @@ class StartUITest {
         Output output = new ConsoleOutput();
         String replacedName = "New item name";
         Input input = new MockInput(new String[] {"0", String.valueOf(item.getId()), replacedName, "1"});
-        UserAction[] actions = {new ReplaceAction(output), new ExitAction(output)};
+        UserAction[] actions = {new Replace(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
@@ -34,7 +35,7 @@ class StartUITest {
         Item item = tracker.add(new Item("Deleted item"));
         Output output = new ConsoleOutput();
         Input input = new MockInput(new String[] {"0", String.valueOf(item.getId()), "1"});
-        UserAction[] actions = {new DeleteAction(output), new ExitAction(output)};
+        UserAction[] actions = {new Delete(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
     }
@@ -46,7 +47,7 @@ class StartUITest {
         Item two = tracker.add(new Item("Second item"));
         Output output = new StubOutput();
         Input input = new MockInput(new String[] {"0", "1"});
-        UserAction[] actions = {new FindAllAction(output), new ExitAction(output)};
+        UserAction[] actions = {new FindAll(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(output.toString()).isEqualTo(
@@ -70,7 +71,7 @@ class StartUITest {
         Item two = tracker.add(new Item("Second item"));
         Output output = new StubOutput();
         Input input = new MockInput(new String[] {"0", two.getName(), "1"});
-        UserAction[] actions = {new FindByNameAction(output), new ExitAction(output)};
+        UserAction[] actions = {new FindByName(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(output.toString()).isEqualTo(
@@ -93,7 +94,7 @@ class StartUITest {
         Item two = tracker.add(new Item("Second item"));
         Output output = new StubOutput();
         Input input = new MockInput(new String[] {"0", String.valueOf(one.getId()), "1"});
-        UserAction[] actions = {new FindByIdAction(output), new ExitAction(output)};
+        UserAction[] actions = {new FindById(output), new Exit(output)};
         new StartUI(output).init(input, tracker, actions);
         String ln = System.lineSeparator();
         assertThat(output.toString()).isEqualTo(
