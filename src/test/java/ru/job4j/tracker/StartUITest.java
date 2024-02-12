4472ea4ad9b2cb5adc94b484abba4b730912e6eb
Nurlan <001nurlan@gmail.com>
2024-02-12 16:23:55 +0600

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -127,4 +127,44 @@ class StartUITest {
                         + "=== Завершение программы ===" + ln
         );
     }
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[] {"one", "1"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[] {"5"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(5);
+    }
+
+    @Test
+    void whenMultipleValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[] {"5", "4", "11"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(5);
+        int selected1 = input.askInt("Enter menu");
+        assertThat(selected1).isEqualTo(4);
+        int selected2 = input.askInt("Enter menu");
+        assertThat(selected2).isEqualTo(11);
+    }
+
+    @Test
+    void whenValidMinusInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(new String[] {"-5", "4", "11"});
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu");
+        assertThat(selected).isEqualTo(-5);
+    }
 }
