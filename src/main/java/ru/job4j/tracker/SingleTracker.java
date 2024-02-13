618bc695d25c1985ba19191f7349f51981a57894
Nurlan <001nurlan@gmail.com>
2024-02-13 22:48:11 +0600

    1. Singleton [#94606 #480985]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
+    private Tracker tracker = new Tracker();
+
     private static SingleTracker instance = null;
 
     private SingleTracker() {
@@ -12,8 +14,6 @@ public final class SingleTracker {
         }
         return instance;
     }
-
-    private Tracker tracker = new Tracker();
 
     public Item add(Item item) {
         return tracker.add(item);
