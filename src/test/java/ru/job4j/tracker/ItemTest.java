b07b1a0223c6f012fa2d73dcbf7aa885d8798a26
Nurlan <001nurlan@gmail.com>
2024-03-06 20:53:03 +0600

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.ItemDescByName;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemTest {
+
+    @Test
+    void whenIncreaseSort() {
+        List<Item> expected = Arrays.asList(new Item(5, "A"),
+                                            new Item(8, "B"),
+                                            new Item(1, "C"),
+                                            new Item(4, "D"));
+        List<Item> items = Arrays.asList(new Item(4, "D"),
+                                        new Item(5, "A"),
+                                        new Item(1, "C"),
+                                        new Item(8, "B"));
+        items.sort(new ItemAscByName());
+        assertThat(items.equals(expected)).isTrue();
+    }
+
+    @Test
+    void whenDecreaseSort() {
+        List<Item> expected = Arrays.asList(new Item(5, "D"),
+                                            new Item(8, "C"),
+                                            new Item(1, "B"),
+                                            new Item(4, "A"));
+        List<Item> items = Arrays.asList(new Item(4, "D"),
+                                        new Item(5, "A"),
+                                        new Item(1, "C"),
+                                        new Item(8, "B"));
+        items.sort(new ItemDescByName());
+        assertThat(items.equals(expected)).isTrue();
+    }
+}
