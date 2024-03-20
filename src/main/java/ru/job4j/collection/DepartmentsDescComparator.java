573b387b0397dd5f822438eeeb75135ded0be003
Nurlan <001nurlan@gmail.com>
2024-03-20 13:03:51 +0600

    3. Отсортировать департаменты [#34352 #486884]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -5,17 +5,9 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result;
         String[] leftElements = left.split("/", 2);
         String[] rightElements = right.split("/", 2);
-        int length = Math.min(leftElements.length, rightElements.length);
-        result = rightElements[0].compareTo(leftElements[0]);
-        if (result == 0 && length > 1) {
-            return leftElements[1].compareTo(rightElements[1]);
-        } else if (result == 0) {
-            return Integer.compare(leftElements.length, rightElements.length);
-        } else {
-            return result;
-        }
+        int result = rightElements[0].compareTo(leftElements[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
