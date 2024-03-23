dac2ac5671b169c93933dc7d7bb7d9678dd33dc0
Nurlan <001nurlan@gmail.com>
2024-03-23 21:10:24 +0600

    2.4. Отложенное исполнение лямбды [#249211]

---
+++ src/main/java/ru/job4j/function/DeffExecution.java
@@ -0,0 +1,15 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class DeffExecution {
+    public static void main(String[] args) {
+        String[] names = {"Ivan", "Petr"};
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("execute comparator");
+            return Integer.compare(left.length(), right.length());
+        };
+        Arrays.sort(names, comparator);
+    }
+}
